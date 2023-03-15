d6be5f510a9e4597c3438231e47375040d6576e9
Denys Kolomiiets <kolomietzd@gmail.com>
2023-03-16 01:19:27 +0200

    add modal windows+scripts

--- src/js/script.js
+++ src/js/script.js
@@ -36,4 +36,25 @@ $(document).ready(function(){
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         });
     });
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    // $('.button_mini').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+
+    $('.button_mini').each(function(i){   //скрипт, позволяющий вытянуть название товара и вставить его в модальное окно
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
   });
+
+ 
