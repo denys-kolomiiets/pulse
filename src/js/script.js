7ccf457db9af955395b62cb74c1658e293d00b4f
Denys Kolomiiets <kolomietzd@gmail.com>
2023-03-16 12:39:28 +0200

    add tel mask in modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -114,6 +114,10 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $('input[name=phone]').mask("+38(999)999-99-99");
+
+
     
   });
 
